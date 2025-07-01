13b92b15072b821652dc97b415b2ac173d8ecfb7
MiguelwLD <114840612+Miguel7293@users.noreply.github.com>
2025-07-01 17:29:15 -0500

    Siuuuuuuux2

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 import ProductCard from './ProductCard';
 
 const sampleProducts = [
-  { id: 1, name: 'Auriculares Bluetooth', price: 89.99, image: 'https://i.imgur.com/Rx0wwWj.png' },
-  { id: 2, name: 'Teclado Mecánico RGB', price: 129.99, image: 'https://i.imgur.com/dFZpQBu.png' },
-  { id: 3, name: 'Mouse Gamer', price: 49.99, image: 'https://i.imgur.com/Gq3z6nH.png' },
-  { id: 4, name: 'Monitor Curvo 27"', price: 199.99, image: 'https://i.imgur.com/hLE4HnP.png' },
+  { id: 1, name: 'Auriculares Bluetooth', price: 89.99, image: 'https://oechsle.vteximg.com.br/arquivos/ids/8590434/imageUrl_2.jpg?v=637874930055000000' },
+  { id: 2, name: 'Teclado Mecánico RGB', price: 129.99, image: 'https://promart.vteximg.com.br/arquivos/ids/7479097-1000-1000/image-9af968446b50480fade90869c612e40f.jpg?v=638305829355370000' },
+  { id: 3, name: 'Mouse Gamer', price: 49.99, image: 'https://media.falabella.com/falabellaPE/125240618_01/w=1500,h=1500,fit=pad' },
+  { id: 4, name: 'Monitor Curvo 27"', price: 199.99, image: 'https://media.falabella.com/falabellaPE/129011443_01/w=800,h=800,fit=pad' },
 ];
 
 const ProductList = () => {
